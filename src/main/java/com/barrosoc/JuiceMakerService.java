2361fb42365779e251c8891290d259224cee889d
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2022-12-06 10:48:29 +0100

    Add logs in JuiceMakerService when making the juice

--- src/main/java/com/barrosoc/JuiceMakerService.java
+++ src/main/java/com/barrosoc/JuiceMakerService.java
@@ -1,10 +1,16 @@
 package com.barrosoc;
 
-import javax.enterprise.context.ApplicationScoped;
+import static org.apache.commons.lang3.StringUtils.capitalize;
 
+import javax.enterprise.context.ApplicationScoped;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @ApplicationScoped
 public class JuiceMakerService {
 
     public void makeJuice(String fruit) {
+        log.info("Making %s juice...".formatted(fruit));
+        log.info("%s juice done :)".formatted(capitalize(fruit)));
     }
 }
